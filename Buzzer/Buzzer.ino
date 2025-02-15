14a0a83ae45606abc0d5c07ad6e7ca5e8693042a
islandking929 <xu.ryanw@gmail.com>
2025-02-15 18:26:03 -0500

    Update Buzzer.ino

--- Buzzer/Buzzer.ino
+++ Buzzer/Buzzer.ino
@@ -1,52 +1,94 @@
 int buzzer_Pin_1 = 9;
+int buzzer_Pin_2 = 10;
+int buzzer_Pin_3 = 11;
+int buzzer_Pin_4 = 12;
+int buzzer_Pin_5 = 13;
 int buttonPin = 2;
 int buttonState;
-int delayTime = 500;
-int i =0;
+//int delayTime = 500;
+int i =9;
 
 void setup() {
-  while i < 
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  while (i < 14){
+    pinMode(i, OUTPUT);
+    i += 1;
+
+
+  }
 
 
 }
-
+void buzzer_1(){
+  tone(buzzer_Pin_1, 440,200);
+  noTone(buzzer_Pin_1);
+  //plays octave 4 A
+}
+void buzzer_2(){
+  tone(buzzer_Pin_2,262, 200);
+  noTone(buzzer_Pin_2);
+  //plays octave 4 c
+}
+void buzzer_3(){
+  tone(buzzer_Pin_3, 330, 200);
+  noTone(buzzer_Pin_3);
+  //plays octave 4 e
+}
+void buzzer_4(){
+  tone(buzzer_Pin_4, 392, 200);
+  noTone(buzzer_Pin_4);
+  //plays octave 4 G
+}
+void buzzer_5(){
+  tone(buzzer_Pin_5, 294, 200);
+  noTone(buzzer_Pin_5);
+  //plays octave 4 5
+}
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (True) {
-    tone(8, 440);
-    delay(1000);
-    noTone(8);
-    delay(22000);
+  
+tone(buzzer_Pin_1, 294, 200);
+delay(200);
+tone(buzzer_Pin_1, 392, 200);
+delay(200);
 
-    tone(8, 594);
-    delay(1000);
-    noTone(8);
-    delay(2000);
-    tone(8, 523);
-    delay(2000);
-    noTone(8);
+tone(buzzer_Pin_1, 330, 200);
+delay(200);
+tone(buzzer_Pin_1,262, 200);
+delay(200);
+tone(buzzer_Pin_1, 440,200);
+delay(200);
+  // buttonState = digitalRead(buttonPin);
+  // if (true) {
+  //   tone(8, 440);
+  //   delay(1000);
+  //   noTone(8);
+  //   delay(22000);
 
-    digitalWrite(9, HIGH);
-    delay(delayTime);
-    tone(buzzerPin, 587);
-    delay(delayTime);
-    tone(buzzerPin, 659);
-    delay(delayTime);
-    tone(buzzerPin, 659);
-    delay(delayTime);
-    tone(buzzerPin, 587);
-    delay(delayTime);
-    tone(buzzerPin, 523);
-    delay(delayTime);
-    tone(buzzerPin, 494);
-    delay(delayTime);
-    tone(buzzerPin, 440);
-    delay(delayTime);
-  } 
-  else {
-   noTone(buzzerPin);
-  }
+  //   tone(8, 594);
+  //   delay(1000);
+  //   noTone(8);
+  //   delay(2000);
+  //   tone(8, 523);
+  //   delay(2000);
+  //   noTone(8);
+
+  //   digitalWrite(9, HIGH);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 587);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 659);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 659);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 587);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 523);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 494);
+  //   delay(delayTime);
+  //   tone(buzzerPin, 440);
+  //   delay(delayTime);
+  // } 
+  // else {
+  //  noTone(buzzerPin);
+  // }
 }
